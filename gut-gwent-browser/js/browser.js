c353257a09732eeac7a99ab69effd760a874a840
AleksanderGondek <gondekaleksander@hotmail.com>
2016-06-12 15:48:12 +0200

    Everything is working, only data is lacking

--- gut-gwent-browser/js/browser.js
+++ gut-gwent-browser/js/browser.js
@@ -28,13 +28,20 @@ app.controller('mainCtrl', function ($scope, cards) {
     $scope.applicationViews = ['Cards', 'Deck', 'About'];
     $scope.currentView = undefined;
     $scope.cards = [];
+    $scope.cards2 = [];
     $scope.cardDetails = undefined;
     $scope.cardsSelected = [];
+    $scope.cardsSelected2 = [];
 
     $scope.changeCurrentView = function(viewName) {
         for (var i = 0; i < $scope.applicationViews.length; i++) {
             if ($scope.applicationViews[i] === viewName) {
                 $scope.currentView = $scope.applicationViews[i];
+
+                // If it is 'Deck'
+                if ($scope.currentView === $scope.applicationViews[1]) {
+                    $scope.onMenuDeckClick();
+                }
                 break;
             }
         }
@@ -57,8 +64,27 @@ app.controller('mainCtrl', function ($scope, cards) {
         }
     }
 
-    $scope.test = function() {
-        return angular.toJson($scope.cardsSelected, true);
+    $scope.setCardPreview = function(fieldObjectPrefix, cardDetails) {
+        var cardDetailsImage = document.getElementById(fieldObjectPrefix + "Image");
+        cardDetailsImage.src = cardDetails.imageBig;
+        var cardDetailsName = document.getElementById(fieldObjectPrefix + "Name");
+        cardDetailsName.innerHTML = cardDetails.name;
+        var cardDetailsFaction = document.getElementById(fieldObjectPrefix + "Faction");
+        cardDetailsFaction.innerHTML = cardDetails.faction;
+        var cardDetailsNameStrength = document.getElementById(fieldObjectPrefix + "Strength");
+        cardDetailsNameStrength.innerHTML = cardDetails.strength;
+        var cardDetailsAbilities = document.getElementById(fieldObjectPrefix + "Abilities");
+        cardDetailsAbilities.innerHTML = cardDetails.abilities;
+        var cardDetailsIsHero = document.getElementById(fieldObjectPrefix + "IsHero");
+        cardDetailsIsHero.innerHTML = cardDetails.isHero;
+    };
+
+    $scope.onMenuDeckClick = function () {
+        $scope.cards2 = [];
+
+        for (var i = 0; i < $scope.cardsSelected.length; i++) {
+            $scope.cards2.push(angular.fromJson(angular.toJson($scope.cards[$scope.cardsSelected[i]], false)));
+        }
     };
 
     $scope.init = function() {
@@ -73,20 +99,21 @@ app.controller('mainCtrl', function ($scope, cards) {
             setTimeout(function() {
                 e.detail.itemPromise.then(function (invokedItem) {
                     var listFlyout = document.getElementById("listItemFlyout");
+                    $scope.setCardPreview("cardDetails", invokedItem.data);
+                    listFlyout.winControl.show(target);
+                });
+            }, 0);
+        });
 
-                    var cardDetailsImage = document.getElementById("cardDetailsImage");
-                    cardDetailsImage.src = invokedItem.data.imageBig;
-                    var cardDetailsName = document.getElementById("cardDetailsName");
-                    cardDetailsName.innerHTML = invokedItem.data.name;
-                    var cardDetailsFaction = document.getElementById("cardDetailsFaction");
-                    cardDetailsFaction.innerHTML = invokedItem.data.faction;
-                    var cardDetailsNameStrength= document.getElementById("cardDetailsStrength");
-                    cardDetailsNameStrength.innerHTML = invokedItem.data.strength;
-                    var cardDetailsAbilities = document.getElementById("cardDetailsAbilities");
-                    cardDetailsAbilities.innerHTML = invokedItem.data.abilities;
-                    var cardDetailsIsHero = document.getElementById("cardDetailsIsHero");
-                    cardDetailsIsHero.innerHTML = invokedItem.data.isHero;
-
+        // Previewing card details in listView2
+        document.getElementById("listView2").addEventListener("iteminvoked", function (e) {
+            var target = e.target;
+            // ListView appears to be programmatically stealing focus on pointer up (or some other event), which causes a light dismissal of the Flyout
+            // This is a WA
+            setTimeout(function () {
+                e.detail.itemPromise.then(function (invokedItem) {
+                    var listFlyout = document.getElementById("listItemFlyout");
+                    $scope.setCardPreview("cardDetails", invokedItem.data);
                     listFlyout.winControl.show(target);
                 });
             }, 0);
--- gut-gwent-browser/js/listViewsResize.js
+++ gut-gwent-browser/js/listViewsResize.js
@@ -12,4 +12,18 @@
 
         listviewdiv.setAttribute("style", "height:" + h.toString() + "px");
     }, true).done();
+
+    window.addEventListener("resize", function () {
+        var listviewdiv = document.getElementById("listView2");
+        var h = window.innerHeight;
+
+        listviewdiv.setAttribute("style", "height:" + h.toString() + "px");
+    });
+
+    WinJS.Utilities.ready(function () {
+        var listviewdiv = document.getElementById("listView2");
+        var h = window.innerHeight;
+
+        listviewdiv.setAttribute("style", "height:" + h.toString() + "px");
+    }, true).done();
 })();
